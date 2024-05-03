30e9f918a22459d799af1e8f545a23a15f5481a2
garcia.dev <erikgarcia.contato@gmail.com>
2024-05-02 22:05:25 -0300

    Tipos-de-Variaveis-finalizado

--- tipos-variaveis/src/TiposVariasveis.java
+++ tipos-variaveis/src/TiposVariasveis.java
@@ -1,5 +1,26 @@
 public class TiposVariasveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    //    double salarioMinimo = 2500;
+          
+        String meuNome = "ERIK GARCIA";
+          
+         
+          short numeroCurto = 1;
+          int numeroNormal = numeroCurto;
+          short numeroCurto2 = (short) numeroNormal;
+      
+
+
+
+         int numero = 1;
+          
+          numero = 2;
+          System.out.println(numero);
+
+        //    double VALOR_DE_PI = 3.14;
+          final double VALOR_DE_PI = 3.14;
+
+        //   VALOR_DE_PI = 10.75;
+          System.out.println(VALOR_DE_PI);
     }
-}
+    }
